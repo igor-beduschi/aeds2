6a4ce3014dda86706a634686015946fd5e1c87bd
inside4 <igor.beduschi@outlook.com>
2020-03-03 19:18:22 -0300

    Classe99%

--- TP02/Classe_Java_Q1/Classe.java
+++ TP02/Classe_Java_Q1/Classe.java
@@ -1,128 +1,167 @@
 import java.io.*;
 import java.util.Scanner;
 
+class Personagens{
+    private String nome;
+    private int altura;
+    private double peso;
+    private String corDoCabelo;
+    private String corDaPele;
+    private String corDosOlhos;
+    private String anoNascimento;
+    private String genero;
+    private String homeworld;
+
+    Personagens(){
+        this.nome = "";
+        this.altura = 0;
+        this.peso = 0.0;
+        this.corDaPele = "";
+        this.corDoCabelo = "";
+        this.corDosOlhos = "";
+        this.anoNascimento = "";
+        this.genero = "";
+        this.homeworld = "";
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getAltura() {
+        return this.altura;
+    }
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+    public double getPeso() {
+        return this.peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    public String getCorDoCabelo() {
+        return this.corDoCabelo;
+    }
+    public void setCorDoCabelo(String corDoCabelo) {
+        this.corDoCabelo = corDoCabelo;
+    }
+    public String getCorDaPele() {
+        return this.corDaPele;
+    }
+    public void setCorDaPele(String corDaPele) {
+        this.corDaPele = corDaPele;
+    }
+    public String getCorDosOlhos() {
+        return this.corDosOlhos;
+    }
+    public void setCorDosOlhos(String corDosOlhos) {
+        this.corDosOlhos = corDosOlhos;
+    }
+    public String getAnoNascimento() {
+        return this.anoNascimento;
+    }
+    public void setAnoNascimento(String anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+    public String getGenero() {
+        return this.genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getHomeworld() {
+        return this.homeworld;
+    }
+    public void setHomeworld(String homeworld) {
+        this.homeworld = homeworld;
+    }
+    public void lerArquivo(String nomeArquivo){
+        try {
+            FileReader arq = new FileReader(nomeArquivo);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = lerArq.readLine();
+            linha = linha.replaceAll("\'","");
+
+            int namePos = linha.indexOf("name");
+            int nameFim = linha.indexOf(",");
+            this.nome = linha.substring(namePos+6,nameFim);
+
+            int heightPos = linha.indexOf("height");
+            int heightFim = linha.indexOf(",",heightPos);
+            String alturaLine = linha.substring(heightPos+8,heightFim);
+            if(!alturaLine.equals("unknown")) this.altura = Integer.parseInt(alturaLine);
+
+            int massPos = linha.indexOf("mass");
+            int massFim = linha.indexOf(",",massPos);
+            String massLine = linha.substring(massPos+6,massFim);
+            if(!massLine.equals("unknown")) this.peso = Double.parseDouble(massLine);
+
+            int cabeloPos = linha.indexOf("hair_color");
+            int cabeloFim = linha.indexOf(",",cabeloPos);
+            this.corDoCabelo = linha.substring(cabeloPos+12,cabeloFim);
+
+            int pelePos = linha.indexOf("skin_color");
+            int peleFim = linha.indexOf("eye_color",pelePos);
+            this.corDaPele = linha.substring(pelePos+12,peleFim-2);
+
+            int olhoPos = linha.indexOf("eye_color");
+            int olhoFim = linha.indexOf(",",olhoPos);
+            this.corDosOlhos = linha.substring(olhoPos+11,olhoFim);
+
+            int nasciPos = linha.indexOf("birth_year");
+            int nasciFim = linha.indexOf(",",nasciPos);
+            this.anoNascimento = linha.substring(nasciPos+12,nasciFim);
+
+            int genderPos = linha.indexOf("gender");
+            int genderFim = linha.indexOf(",",genderPos);
+            this.genero = linha.substring(genderPos+8,genderFim);
+
+            int planetaPos = linha.indexOf("homeworld");
+            int planetaFim = linha.indexOf(",",planetaPos);
+            this.homeworld = linha.substring(planetaPos+11,planetaFim);
+
+            Printar();
+        } catch (IOException e) {}
+    }
+
+    public void Printar(){
+        System.out.printf(" ## %s ## %d ## ",this.nome,this.altura);
+        if(this.peso == (long) this.peso) System.out.printf("%d ## ",(long) this.peso);
+        else System.out.printf("%s ## ",this.peso);
+        System.out.printf("%s ## %s ## %s ## %s ## %s ## %s ##\n",
+        this.corDoCabelo,this.corDaPele,this.corDosOlhos,this.anoNascimento,this.genero,this.homeworld);
+       
+    }
+}
+
 class Classe {
+       
+    public static boolean isFim(String s){
+        return (s.length() >= 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
 
-    public class Personagens{
-        private String nome;
-        private int altura;
-        private double peso;
-        private String corDoCabelo;
-        private String corDaPele;
-        private String corDosOlhos;
-        private String anoNascimento;
-        private String genero;
-        private String homeworld;
+    public static String ISO88591toUTF8(String strISO){
+        try{
+            byte[] isoBytes = strISO.getBytes("ISO-8859-1");
+            return new String(isoBytes, "UTF-8");
+        } catch (IOException e) {}
+        return strISO;
+    }
 
-        public String getNome() {
-            return this.nome;
+    public static void main (String[] args){
+        String[] entrada = new String[1000];
+        Personagens personagem[] = new Personagens[1000];
+        int numEntrada = 0;
+        do {
+            entrada[numEntrada] = MyIO.readLine();
+            entrada[numEntrada] = ISO88591toUTF8(entrada[numEntrada]);
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--;
+        for(int i = 0; i < numEntrada; i++){
+            personagem[i] = new Personagens();
+            personagem[i].lerArquivo(entrada[i]);
         }
-
-        public void setNome(String nome) {
-            this.nome = nome;
-        }
-
-        public int getAltura() {
-            return this.altura;
-        }
-
-        public void setAltura(int altura) {
-            this.altura = altura;
-        }
-
-        public double getPeso() {
-            return this.peso;
-        }
-
-        public void setPeso(double peso) {
-            this.peso = peso;
-        }
-
-        public String getCorDoCabelo() {
-            return this.corDoCabelo;
-        }
-
-        public void setCorDoCabelo(String corDoCabelo) {
-            this.corDoCabelo = corDoCabelo;
-        }
-
-        public String getCorDaPele() {
-            return this.corDaPele;
-        }
-
-        public void setCorDaPele(String corDaPele) {
-            this.corDaPele = corDaPele;
-        }
-
-        public String getCorDosOlhos() {
-            return this.corDosOlhos;
-        }
-
-        public void setCorDosOlhos(String corDosOlhos) {
-            this.corDosOlhos = corDosOlhos;
-        }
-
-        public String getAnoNascimento() {
-            return this.anoNascimento;
-        }
-
-        public void setAnoNascimento(String anoNascimento) {
-            this.anoNascimento = anoNascimento;
-        }
-
-        public String getGenero() {
-            return this.genero;
-        }
-
-        public void setGenero(String genero) {
-            this.genero = genero;
-        }
-
-        public String getHomeworld() {
-            return this.homeworld;
-        }
-
-        public void setHomeworld(String homeworld) {
-            this.homeworld = homeworld;
-        }
-
     }
 }
-
-public void lerTime()throws Exception
-{
-    FileReader arq = new FileReader(this.nomeArquivo);
-    BufferedReader br = new BufferedReader(arq);
-    String line = "";
-    line = br.readLine();
-    String html = "";
-    while(!line.contains("vcard"))
-    {
-        line = br.readLine();
-    }
-    br.close();
-    html += line;
-    html = html.substring(html.indexOf("Full name"));
-
-    setPaginaTam( tamanho() );
-    System.out.println(fullname(html)+" ## "+nickname(html)+" ## "+founded(html)+" ## "+ground(html)+" ## "+capacity(html)+" ## "+coach(html)+" ## "+league(html)+" ## "+this.nomeArquivo+" ## "+this.paginaTam+" ## ");
-}
-
-public static boolean isFim(String s){
-    return (s.length() >= 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
-}
-
-public static void main (String[] args){
-   String[] entrada = new String[1000];
-   int numEntrada = 0;
-   //Leitura da entrada padrao
-   do {
-     entrada[numEntrada] = MyIO.readLine();
-   } while (isFim(entrada[numEntrada++]) == false);
-   numEntrada--;   //Desconsiderar ultima linha contendo a palavra FIM
-   //Para cada linha de entrada, gerando uma de saida contendo o numero de letras maiusculas da entrada
-   for(int i = 0; i < numEntrada; i++){
-     MyIO.println(entrada[i]);
-   }
-}
-
